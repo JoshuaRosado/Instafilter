590741db0a842ec343e3284e4559a9b667fab5bf
JoshuaRosado <joshua06rosado@gmail.com>
2025-05-31 09:17:25 -0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Joshua Rosado Olivencia on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstafilterTests/InstafilterTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterTests.swift
+//  InstafilterTests
+//
+//  Created by Joshua Rosado Olivencia on 5/31/25.
+//
+
+import Testing
+@testable import Instafilter
+
+struct InstafilterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

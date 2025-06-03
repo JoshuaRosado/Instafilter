fda0d698e680720aa0a163ade8f5a0a2f79b7fce
JoshuaRosado <joshua06rosado@gmail.com>
2025-06-03 13:48:15 -0500

    Tracking the @State updates using onChange()

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,23 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0{
-        // Do this when value updates
-        didSet {
-            print("New value is \(blurAmount)")
-        }
-        
-    }
+    @State private var blurAmount = 0.0
+    
     var body: some View {
         VStack {
             Text("Hello, world!")
                 .blur(radius: blurAmount)
-            
+            //
             Slider(value: $blurAmount, in : 0...20)
-            
-            Button("Random Blur"){
-                blurAmount = Double.random(in: 0...20)
-            }
+            // When blurAmount updates,  return old and new value
+            // and print new value
+                .onChange(of: blurAmount) {oldValue, newValue in
+                    print("New value is \(newValue)")
+                }
         }
         
     }

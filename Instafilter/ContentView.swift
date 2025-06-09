eb0289c2eeb8b0357ff600a747b1de92ebccbb1f
JoshuaRosado <joshua06rosado@gmail.com>
2025-06-09 08:29:36 -0500

    Core Image steps

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,41 +5,65 @@
 //  Created by Joshua Rosado Olivencia on 5/31/25.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
+
+
+// To create a Core Image context and a Core Image filter.
+import CoreImage // x
+import CoreImage.CIFilterBuiltins // x
 import SwiftUI
+
+
+
+// UIImage = UIKit
+// CGImage = Core Graphics
+// CIImage = Core Image
+
+// UIImage = Extreme powerful image type capable of working with a variety of image types(PNG, SVG) and even sequences that form an animation. UIImage is the standard imge type for UIKit, and of the three it's the closest to SwiftUI's Image type.
+
+// CGImage = which comes from Core Graphics. This i a simpler image type that is really just a two-dimensional array of pixels.
+
+// CIImage = which comes from Core Image. This stores all the information required to produce an image but doesn't actually turn that pixels unless it's asked to. Apple calls CIImage " an image recipe" rather than an actual image.
+
+
 
 struct ContentView: View{
     // Use optional Image
     @State private var image: Image?
     var body: some View {
         VStack{
-            image?
+            image? // If there's an image, display it
+            //modifiers
                 .resizable()
+                .scaledToFit()
                 
                 
         }
         .ignoresSafeArea()
-        .onAppear(perform: loadImage)
+        // When View appears perform loadImage method
+        .onAppear(perform: loadImage) // If there's no existing image, it won't trigger the onAppear() function
+        
     }
-    
+    // Loading method
     func loadImage(){
-        // load our Image into a UIImage
+        // Grab our image
+        // Load our Image into a UIImage
         let inputImage = UIImage(resource: .italy)
-        // put inputImage into a CIImage
+        
+        // Manipulate it using Core Image
+        // This is what Core Image wants to work with
         let beginImage = CIImage(image: inputImage)
         
         
-        // make our context
+        // Create a context
         let context = CIContext()
-        // make our filter, select our filter
-        let currentFilter = CIFilter.dither()
+        // Create our filter and select
+        let currentFilter = CIFilter.dither() //CIFilter."Filter Selected"
         
-        //
+        // Customize our filter
         currentFilter.inputImage = beginImage
-        currentFilter.intensity = 0.5
+        currentFilter.intensity = 0.3
         
-        // guard = exit before it fails
+        // Guard = exit before it fails
         guard let outputImage = currentFilter.outputImage else { return}
         
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent)

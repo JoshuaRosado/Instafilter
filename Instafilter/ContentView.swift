3ff6f3094b629bd9fa57c4e1769f5d84b3a31aad
JoshuaRosado <joshua06rosado@gmail.com>
2025-06-17 13:19:50 -0500

    Update

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -14,13 +14,15 @@ struct ContentView: View {
     var body: some View {
         
         // *Sharing URL
+        // Custom Label with message
         
-//        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!){
-//            Label("Spread the word about hacking with swift", systemImage: "swift")
-//        }
+        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!){
+            Label("Spread the word about hacking with swift", systemImage: "swift")
+        }
         
         
         // *Sharing Image
+        // Sharing complex data
         
         ShareLink(item: example, preview: SharePreview("Italy", image: example)){
             // Customizing the message

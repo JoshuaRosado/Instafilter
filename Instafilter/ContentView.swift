b0a04905494325f1c7d3cfac70082da07d5ca565
JoshuaRosado <joshua06rosado@gmail.com>
2025-06-17 13:36:37 -0500

    3 Steps using requestReview

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,32 +5,25 @@
 //  Created by Joshua Rosado Olivencia on 5/31/25.
 //
 
+// STEP 1
+import StoreKit //*
 import SwiftUI
 
 struct ContentView: View {
-    // Sharing image
-    let example = Image(.italy)
-    
+    // STEP 2
+    // ADD Property to read the review requester from SwiftUI's environment
+    @Environment(\.requestReview) var requestReview
     var body: some View {
+        Text("Instafilter")
         
-        // *Sharing URL
-        // Custom Label with message
+        // STEP 3
+        // Need to request a review when ready
         
-        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!){
-            Label("Spread the word about hacking with swift", systemImage: "swift")
-        }
-        
-        
-        // *Sharing Image
-        // Sharing complex data
-        
-        ShareLink(item: example, preview: SharePreview("Italy", image: example)){
-            // Customizing the message
-            Label("Share", systemImage: "airplane")
+        Button("Leave a review"){
+            requestReview()
         }
     }
 }
-
 #Preview {
     ContentView()
 }
